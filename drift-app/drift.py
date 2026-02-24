2bd45a756459f00bfb7d538eb016606b3f0eb8d2
jiangau <autumnriver1112@gmail.com>
2026-02-24 21:01:49 +0700

    Make sure the app can run with tested data

--- drift-app/drift.py
+++ drift-app/drift.py
@@ -8,3 +8,14 @@ load_dotenv()
 
 app = Flask(__name__)
 client = HumeClient(api_key=os.getenv("HUME_API_KEY"))
+
+job_id = client.expression_measurement.batch.start_inference_job(
+    urls=["https://hume-tutorials.s3.amazonaws.com/faces.zip"],
+    models=Models(
+        prosody=Prosody(granularity="utterance"),
+    ),
+)
+print(f"Job ID: {job_id}")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
