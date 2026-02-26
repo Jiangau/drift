c7fd1dd19266647636cd2a3c5ffe2d5e20db254f
jiangau <autumnriver1112@gmail.com>
2026-02-26 12:08:54 +0700

    Degbug, adding a GET method to my function

--- drift-app/drift.py
+++ drift-app/drift.py
@@ -3,34 +3,31 @@ from hume import HumeClient
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 import os
-import io
 import time
 
 load_dotenv()
 
 app = Flask(__name__)
 client = HumeClient(api_key=os.getenv("HUME_API_KEY"))
-print(os.getenv("HUME_API_KEY"))
 
-@app.route('/analyze', methods=["POST"])
+
+@app.route('/analyze', methods=['GET','POST'])
 def sendAudio():
     try:
         if 'file' not in request.files:
             return jsonify({"error: No file"}),400
 
         audioFile = request.files['file']
-        fileContent = audioFile.read()
         
         jobResponse = client.expression_measurement.batch.start_inference_job(
-            [(audioFile.filename, audioFile.stream)],
+            files=[(audioFile.filename, audioFile.stream)],
             models=Models(prosody=Prosody(granularity="utterance"),),
         )
         
-        job_id= jobResponse.id
         
-        print(jsonify(f"Jobstarted. {job_id}"), 200)
+        print(jsonify(f"Jobstarted. {jobResponse}"), 200)
         while True:
-            job_details = client.expression_measurement.batch.get_job_details(id=job_id)
+            job_details = client.expression_measurement.batch.get_job_details(id=jobResponse)
             status = job_details.state.status
 
             if status == "COMPLETED":
@@ -43,7 +40,7 @@ def sendAudio():
             print(f"Status: {status}")
             time.sleep(3)
         
-        result = client.expression_measurement.batch.get_job_predictions(job_id)
+        result = client.expression_measurement.batch.get_job_predictions(jobResponse)
         serializable_result = [r.dict() if hasattr(r, 'dict') else r for r in result]
         return jsonify(serializable_result), 200
     
