459aeb2718bdf2ad6e6c01ad88d5a1b176b5cf17
jiangau <autumnriver1112@gmail.com>
2026-02-27 14:26:48 +0700

    Cleaning up comments

--- drift-app/App.js
+++ drift-app/App.js
@@ -83,13 +83,13 @@ const AudioRecording = () => {
     const formData = new FormData();
     const cleanUri = Platform.OS === 'ios' ? uri.replace('file://','') : uri;
     formData.append('file',{
-      uri: cleanUri,//Platform.OS === 'android' ? audioUri : audioUri.replace('file://',''),
+      uri: cleanUri,
       name: 'recording.m4a',
       type: 'audio/m4a',
     });
 
       console.log("Sending message")
-      const response = await fetch(`http://192.168.1.182:5001/analyze`,{ //:5001/result/${job_id}
+      const response = await fetch(`http://192.168.1.182:5001/analyze`,{ 
         method: 'POST',
         body: formData,
         headers: {'Content-Type': 'application/json'},
