b6675b5277450b5dce2a8fd04ef2fb95f0026a4d
jiangau <autumnriver1112@gmail.com>
2026-03-01 06:19:07 +0700

    Deleting comments, cleaning code

--- drift-app/App.js
+++ drift-app/App.js
@@ -159,25 +159,3 @@ const styles = StyleSheet.create({
 });
 
 export default AudioRecording;
-
-/*
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Working yet.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});*/
