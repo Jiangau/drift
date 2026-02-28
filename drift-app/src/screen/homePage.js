7e2d34c1164ecfaedbca295ca9292e9e4f2470af
jiangau <autumnriver1112@gmail.com>
2026-03-01 06:47:34 +0700

    Including if (isRecording)

--- drift-app/src/screen/homePage.js
+++ drift-app/src/screen/homePage.js
@@ -7,7 +7,16 @@ export default function mainScreen{
     const [emotion, setEmotion] = useState([]);
     const [audioUri, setAudioUri] = useState(null);
     const [loading, setLoading] = useState(false);
+    const {startRecording, stopRecording, isRecording} = recordingAudio();
 
     const controller = async() => {
+        if (isRecording) {
+            setLoading(true);
+            const uri = await stopRecording();
+            const data = await sendingMessage(uri);
+            
+            setEmotion(data);
+            setLoading(false);
+        }
     }
 }
