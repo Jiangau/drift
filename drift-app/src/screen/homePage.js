bb2985f203c6ba023b31000efcf2fc5135c486f2
jiangau <autumnriver1112@gmail.com>
2026-03-01 06:44:28 +0700

    Defining variables

--- drift-app/src/screen/homePage.js
+++ drift-app/src/screen/homePage.js
@@ -3,4 +3,11 @@ import React, { useState, useEffect } from 'react';
 import { sendingMessage } from '../service/sendingMessage';
 import { recordingAudio } from '../hook/recording';
 
-export default function
+export default function mainScreen{
+    const [emotion, setEmotion] = useState([]);
+    const [audioUri, setAudioUri] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const controller = async() => {
+    }
+}
