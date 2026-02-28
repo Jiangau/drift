72955a515edda68b679ecf89792bfc19add1d5cd
jiangau <autumnriver1112@gmail.com>
2026-03-01 06:27:29 +0700

    Transfering the whole sendingMessage to humeCall.js

--- drift-app/src/service/humeCall.js
+++ drift-app/src/service/humeCall.js
@@ -1,4 +1,17 @@
+export const sendingMessage = async(uri) => {
 
+    setLoading(true);
+
+    if (!uri){
+      console.error("NO_URI_");
+      return;
+    };
+
+  try{
+    const filePresence = await fetch(uri);
+    const blob = await filePresence.blob();
+    console.log(blob.size);
+    
     const formData = new FormData();
     const cleanUri = Platform.OS === 'ios' ? uri.replace('file://','') : uri;
     formData.append('file',{
@@ -13,3 +26,13 @@
         body: formData,
         headers: {'Content-Type': 'application/json'},
       });
+
+      const data = await response.json();
+      setEmotion(data);
+      setLoading(false);
+      console.log("Prediction:",JSON.stringify(data,null,2));
+
+    } catch(err){
+      console.error("Cannot send the message",err)
+    };
+  };
